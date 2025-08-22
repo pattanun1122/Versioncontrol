a9ddc884e4e522f36fa6045deea41691d58f4e6f
pattanun1122 <pattnun0@gmail.com>
2025-08-22 19:44:51 +0700

    fix main

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -30,8 +30,8 @@
       <q-toggle v-model="accept" label="I accept the license and terms" />
 
       <div>
-        <q-btn label="ยืนยัน" type="submit" color="primary"/>
-        <q-btn label="เริ่มใหม่" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="ยืนยันครับ" type="submit" color="primary"/>
+        <q-btn label="เริ่มใหม่ครับ" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
 
