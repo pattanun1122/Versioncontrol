1fd36dc52a1ce1a4cefae0ce78c0ad94ca90be21
pattanun1122 <pattnun0@gmail.com>
2025-08-27 17:18:11 +0700

    Edit label warning label null

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -22,7 +22,7 @@
         label="อายุ *"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'อายุ',
+          val => val !== null && val !== '' || 'อายุนะจ๊ะ',
           val => val > 0 && val < 100 || 'อายุเน้อครับ'
         ]"
       />
