3ab2aea50d3c5a47a2574cc7504ee164afc4a0d3
pattanun1122 <pattnun0@gmail.com>
2025-08-27 16:43:57 +0700

    Add template part

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,77 +9,31 @@
       <q-input
         filled
         v-model="name"
-        label="ชื่อ *"
-        hint="ชื่อ นามสกุล"
+        label="Your name *"
+        hint="Name and surname"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'กรุณาพิมบางอย่าง']"
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
 
       <q-input
         filled
         type="number"
         v-model="age"
-        label="อายุ *"
+        label="Your age *"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'กรุณาพิมพ์อายุนะครับ',
-          val => val > 0 && val < 100 || 'กรูราพิมพ์อายุให้ถูกนะจ้ะ'
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
         ]"
       />
 
       <q-toggle v-model="accept" label="I accept the license and terms" />
 
       <div>
-        <q-btn label="ยืนยัน" type="submit" color="primary"/>
-        <q-btn label="เริ่มใหม่" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
 
   </div>
 </template>
-
-<script>
-import { useQuasar } from 'quasar'
-import { ref } from 'vue'
-
-export default {
-  setup () {
-    const $q = useQuasar()
-
-    const name = ref(null)
-    const age = ref(null)
-    const accept = ref(false)
-
-    return {
-      name,
-      age,
-      accept,
-
-      onSubmit () {
-        if (accept.value !== true) {
-          $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'You need to accept the license and terms first'
-          })
-        }
-        else {
-          $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'cloud_done',
-            message: 'Submitted'
-          })
-        }
-      },
-
-      onReset () {
-        name.value = null
-        age.value = null
-        accept.value = false
-      }
-    }
-  }
-}
-</script>
